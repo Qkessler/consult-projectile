e0be9db405412b2280097fd2f0cd52a0e06ef0f6
OlMon <pawlowski.marco@gmail.com>
2021-03-19 18:18:32 +0100

    Added function to use consult find-file for projectile file select

--- consult-projectile.el
+++ consult-projectile.el
@@ -1,23 +1,33 @@
 (require 'projectile)
 (require 'consult)
 
-(setq consult--source-projectile-file
-      `(:name      "Project File"
-                   :narrow    (?f . "File")
-                   :hidden    nil
-                   :category  file
-                   :face      consult-file
-                   :history   file-name-history
-                   :action    ,#'consult--file-action
-                   :enabled   ,(lambda () (and consult-project-root-function
-                                               (projectile-project-root)))
-                   :items
-                   ,(lambda ()
-                      (let*
-                          ((root (projectile-acquire-root))
-                           (inv-root (propertize root 'invisible t))
-                           (files (projectile-project-files root)))
-                        (mapcar (lambda (f) (concat inv-root f)) files)))))
+
+(setq consult--projectile-history nil)
+
+(defcustom consult-projectile-sources
+  '(consult--source-projectile-buffer
+    consult--source-projectile-file
+    consult--source-projectile-project)
+  "Sources used by `consult-projectile'.
+
+See `consult--multi' for a description of the source values."
+  :type '(repeat symbol))
+
+(defun consult-projectile--choose-file (root)
+  (let* ((inv-root (propertize root 'invisible t))
+         (files (projectile-project-files root)))
+    (mapcar (lambda (f) (concat inv-root f)) files)))
+
+(defun consult-projectile--file (selected-project)
+  "Creates a view for selecting project files"
+  (find-file (consult--read
+              (consult-projectile--choose-file selected-project)
+              :prompt "Project File: "
+              :sort nil
+              :require-match t
+              :category 'file
+              :state (consult--file-preview)
+              :history 'file-name-history)))
 
 (setq consult--source-projectile-buffer
       `(:name      "Project Buffer"
@@ -38,6 +48,25 @@
                                                 (string-prefix-p root file)))
                                             (consult--cached-buffers)))))))
 
+(setq consult--source-projectile-file
+      `(:name      "Project File"
+                   :narrow    (?f . "File")
+                   :hidden    nil
+                   :category  file
+                   :face      consult-file
+                   :history   file-name-history
+                   :action    ,#'consult--file-action
+                   :enabled   ,(lambda () (and consult-project-root-function
+                                               (projectile-project-root)))
+                   :items
+                   ,(lambda ()
+                      (let*
+                          ((root (projectile-acquire-root))
+                           (inv-root (propertize root 'invisible t))
+                           (files (projectile-project-files root)))
+                        (mapcar (lambda (f) (concat inv-root f)) files)))))
+
+
 (setq consult--source-projectile-project
       `(:name      "Known Project"
                    :narrow    (?p . "Project")
@@ -45,22 +74,11 @@
                    :category  bookmark
                    :face      consult-bookmark
                    :history   consult--projectile-history
-                   :action    ,#'projectile-switch-project-by-name
+                   :action    ,#'consult-projectile--file ;,#'projectile-switch-project-by-name
                    :enabled   ,(lambda () (and consult-project-root-function))
                    :items
                    ,(lambda ()
                       (projectile-relevant-known-projects))))
-
-(defcustom consult-projectile-sources
-      '(consult--source-projectile-buffer
-        consult--source-projectile-file
-        consult--source-projectile-project)
-      "Sources used by `consult-projectile'.
-
-See `consult--multi' for a description of the source values."
-      :type '(repeat symbol))
-
-(setq consult--projectile-history nil)
 
 ;;;###autoload
 (defun consult-projectile ()
@@ -74,6 +92,7 @@ See `consult--multi' for a description of the source values."
     ;; create a new buffer with the name.
     (unless (cdr buffer)
       (funcall consult--buffer-display (car buffer)))))
+
 
 (provide 'consult-projectile)
 
